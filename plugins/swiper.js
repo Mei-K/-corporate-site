7ae39cc27c7741c912fffd2230f83595bec9d4ee
Mei-K <mei.kikuchi@rakus-partners.co.jp>
2022-04-28 11:09:36 +0900

    swiper

--- plugins/swiper.js
+++ plugins/swiper.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/css/swiper.css'
+// import 'swiper/css/swiper.css'
+import '~/assets/swiper.css'
 
 Vue.use(VueAwesomeSwiper)
